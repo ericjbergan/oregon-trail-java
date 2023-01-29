c5969f96f55fe176ddd33d6077100ad195a997c0
ericjbergan <ericjbergan@gmail.com>
2023-01-29 14:49:08 -0800

    traveler class created; setMarksmanship() done; buyInitialSupplies() done

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -9,61 +9,63 @@ public class Main {
     public static void main(String[] args) {
 
         Scanner sc = new Scanner(System.in);
+        Traveler traveler = new Traveler();
         String instructions = "";
 
         while (!instructions.equalsIgnoreCase("y") && !instructions.equalsIgnoreCase("n")) {
-            System.out.println("DO YOU NEED INSTRUCTIONS  (y/n)");
+            System.out.println("Do you need instructions  (y/n)");
             instructions = sc.nextLine();
         }
 
         if (instructions.equalsIgnoreCase("y")) printInstructions();
 
-        int marksmanship = getMarksmanship(sc);
+        int marksmanship = traveler.setMarksmanship();
 
-        Map<String, Integer> buyOxen = buyOxen(sc);
-        Map<String, Integer> supplyBalances = buyInitialSupplies(buyOxen, sc);
-        System.out.println("AFTER ALL YOUR PURCHASES, YOU NOW HAVE " + supplyBalances.get("cashBalance") +
-                " DOLLARS LEFT");
-        System.out.println("\nJOURNEY BEGINS: MONDAY, MARCH 29, 1847");
+        traveler.buyInitialSupplies();
+        System.out.println("After all your purchases, you now have " +
+                traveler.getSupplyBalances().get("cashBalance") +  " dollars left");
+        System.out.println("\nJourney begins: Monday, March 29, 1847");
 
         pressEnterToContinue();
 
-        travel(supplyBalances, sc, marksmanship, 2040);
+        System.out.println("Good luck!");
+
+        traveler = travel(traveler, sc);
     }
 
     public static void printInstructions() {
         System.out.println("""
-                THIS PROGRAM SIMULATES A TRIP OVER THE OREGON TRAIL FROM INDEPENDENCE, MISSOURI TO OREGON\s
-                CITY, OREGON IN 1847. YOUR FAMILY OF FIVE WILL COVER THE 2040 MILE OREGON TRAIL IN 5-6 MONTHS ---\s
-                IF YOU MAKE IT ALIVE.""");
+                This program simulates a trip over the Oregon Trail from Independence, Missouri to Oregon\s
+                City, Oregon in 1847. Your family of five will cover the 2040 mile Oregon Trail in 5-6 months ---\s
+                if you make it alive.""");
 
         pressEnterToContinue();
 
         System.out.println("""
-                YOU HAD SAVED $900 TO SPEND FOR THE TRIP, AND YOU'VE JUST PAID $200 FOR A WAGON. YOU WILL
-                NEED TO SPEND THE REST OF YOUR MONEY ON THE FOLLOWING ITEMS:""");
-        System.out.println("\nOXEN - YOU CAN SPEND $200-$300 ON YOUR TEAM. THE MORE YOU SPEND, THE FASTER YOU'LL GO " +
-                "BECAUSE YOU'LL HAVE BETTER ANIMALS");
-        System.out.println("\nFOOD - THE MORE YOU HAVE, THE LESS CHANCE THERE IS OF GETTING SICK");
-        System.out.println("\nAMMUNITION - $1 BUYS A BELT OF 50 BULLETS YOU WILL NEED BULLETS FOR ATTACKS BY ANIMALS" +
-                "AND BANDITS, AND FOR HUNTING FOOD");
-        System.out.println("\nCLOTHING - THIS IS ESPECIALLY IMPORTANT FOR THE COLD WEATHER YOU WILL ENCOUNTER WHEN " +
-                "CROSSING THE MOUNTAINS");
-        System.out.println("\nMISCELLANEOUS SUPPLIES - THIS INCLUDES MEDICINE AND OTHER THINGS YOU WILL NEED FOR " +
-                "SICKNESS AND EMERGENCY REPAIRS");
+                You had saved $900 to spend for the trip, and you've just paid $200 for a wagon. You will
+                need to spend the rest of your money on the following items:""");
+        System.out.println("\nOxen - You can spend $200-$300 on your team. the more you spend, the faster you'll go " +
+                "because you have better animals");
+        System.out.println("\nFood - The more you have, the less chance there is of getting sick");
+        System.out.println("\nAmmunition - $1 buys a belt of 50 bullets. You will need bullets for attacks by animals" +
+                "and bandits, and for hunting food");
+        System.out.println("\nClothing - This is especially important for the cold weather you will encounter when " +
+                "crossing the mountains");
+        System.out.println("\nMiscellaneous supplies - This includes medicine and other things you will need for " +
+                "sickness and emergency repairs");
 
         pressEnterToContinue();
 
         System.out.println("""
-                YOU CAN SPEND ALL YOUR MONEY BEFORE YOU START YOUR TRIP OR YOU CAN SAVE SOME OF YOUR\s
-                CASH TO SPEND AT FORTS ALONG THE WAY WHEN YOU RUN LOW. HOWEVER, ITEMS COST MORE AT THE FORTS. YOU\s
-                CAN ALSO GO HUNTING ALONG THE WAY TO GET MORE FOOD.""");
+                You can spend all your money before you start your trip - or you can save some of your\s
+                cash to spend at forts along the way when you run low. However, items cost more at the forts. You \s
+                can also go hunting along the way to get more food.""");
         System.out.println("""
-                WHENEVER YOU HAVE TO USE YOUR TRUSTY RIFLE ALONG THE WAY, YOU WILL BE TOLD TO TYPE IN\s
-                A WORD (ONE THAT SOUNDS LIKE A GUN SHOT). THE FASTER YOU TYPE IN THAT WORD AND HIT THE 'ENTER'\s
-                KEY, THE BETTER LUCK YOU'LL HAVE WITH YOUR GUN.""");
-        System.out.println("\nAT EACH TURN, ALL ITEMS ARE SHOWN IN DOLLAR AMOUNTS EXCEPT BULLETS. WHEN ASKED TO ENTER \n" +
-                "MONEY AMOUNTS, DON'T USE A '$'");
+                Whenever you have to use your trusy rifle along the way, yo uwill be told to type in \s
+                a word (one that sounds like a gun shot). The faster you type in that word and hit the 'enter' \s
+                key, the better luck you'll have with your gun.""");
+        System.out.println("\nAt each turn, all items are shown in dollar amounts except bullets. When asked to enter \n" +
+                "money amounts, don't use a '$'");
 
         pressEnterToContinue();
     }
@@ -74,144 +76,39 @@ public class Main {
         sc.nextLine();
     }
 
-    public static int getMarksmanship(Scanner sc) {
-        sc = new Scanner(System.in);
-        int marksmanship = 0;
-
-        System.out.println("HOW GOOD A SHOT ARE YOU WITH YOUR RIFLE?");
-        System.out.println("(1) ACE MARKSMAN");
-        System.out.println("(2) GOOD SHOT");
-        System.out.println("(3) FAIR TO MIDDLIN'");
-        System.out.println("(4) NEED MORE PRACTICE");
-        System.out.println("(5) SHAKY KNEES");
-        System.out.println("ENTER ONE OF THE ABOVE -- THE BETTER YOU CLAIM YOU ARE, THE FASTER YOU'LL HAVE TO BE " +
-                "WITH YOUR GUN TO BE SUCCESSFUL.");
-
-        while (marksmanship < 1 || marksmanship > 5) {
-            try {
-                String newInput = sc.nextLine();
-                marksmanship = Integer.parseInt(newInput);
-                if (marksmanship < 1 || marksmanship > 5) throw new IllegalArgumentException();
-            } catch (Exception e) {
-                System.out.println("Please enter a number between 1 and 5...");
-            }
-        }
-
-        return marksmanship;
-    }
-
-    public static Map<String, Integer> buyOxen(Scanner sc) {
-        Map<String, Integer> supplyBalances = new HashMap<>();
-        supplyBalances.put("cashBalance", 700);
+    public static void buyInitialSupplies(Scanner sc) {
+        Map<String, Integer> newSupplyBalances = new HashMap<>();
+        newSupplyBalances.put("cashBalance", 700);
         int oxen = -1;
-
-        while (oxen < 200 || oxen > 300) {
-            System.out.println("\nCURRENT CASH BALANCE: " + supplyBalances.get("cashBalance"));
-            System.out.println("\nHOW MUCH DO YOU WANT TO SPEND ON YOUR OXEN TEAM? ENTER BETWEEN 200-300.");
-
-            try {
-                String newInput = sc.nextLine();
-                oxen = Integer.parseInt(newInput);
-                if (oxen < 200 || oxen > 300) throw new IllegalArgumentException();
-                supplyBalances.put("oxen", oxen);
-                supplyBalances.put("cashBalance", supplyBalances.get("cashBalance") - oxen);
-            } catch (Exception e) {
-                System.out.println("Please enter a number between 200 and 300...");
-            }
-        }
-
-        return supplyBalances;
-    }
-
-    public static Map<String, Integer> buyInitialSupplies(Map<String, Integer> supplyBalances, Scanner sc) {
-        Map<String, Integer> newSupplyBalances = new HashMap<>(supplyBalances);
         int food = -1;
         int ammunition = -1;
         int clothing = -1;
         int misc = -1;
 
-        try {
-            food = newSupplyBalances.get("food");
-        } catch (Exception ignored) {
+//        try {
+//            food = newSupplyBalances.get("food");
+//        } catch (Exception ignored) {
+//
+//        }
+//
+//        try {
+//            ammunition = newSupplyBalances.get("ammunition");
+//        } catch (Exception ignored) {
+//
+//        }
+//
+//        try {
+//            clothing = newSupplyBalances.get("clothing");
+//        } catch (Exception ignored) {
+//
+//        }
+//
+//        try {
+//            misc = newSupplyBalances.get("misc");
+//        } catch (Exception ignored) {
+//
+//        }
 
-        }
-
-        try {
-            ammunition = newSupplyBalances.get("ammunition");
-        } catch (Exception ignored) {
-
-        }
-
-        try {
-            clothing = newSupplyBalances.get("clothing");
-        } catch (Exception ignored) {
-
-        }
-
-        try {
-            misc = newSupplyBalances.get("misc");
-        } catch (Exception ignored) {
-
-        }
-
-        System.out.println("\nCURRENT CASH BALANCE: " + supplyBalances.get("cashBalance"));
-        System.out.println("HOW MUCH DO YOU WANT TO SPEND ON FOOD? ENTER BETWEEN 0-" +
-                supplyBalances.get("cashBalance"));
-
-        while (food < 0 || food > supplyBalances.get("cashBalance")) {
-            try {
-                String newInput = sc.nextLine();
-                food = Integer.parseInt(newInput);
-            } catch (Exception e) {
-                System.out.println("Please enter a number between 0 and " + supplyBalances.get("cashBalance"));
-            }
-
-            supplyBalances.put("food", food);
-        }
-
-        while (ammunition < 0 || ammunition > supplyBalances.get("cashBalance")) {
-            System.out.println("\nCURRENT CASH BALANCE: " + supplyBalances.get("cashBalance"));
-            System.out.println("HOW MUCH DO YOU WANT TO SPEND ON AMMUNITION? ENTER BETWEEN 0-" +
-                    supplyBalances.get("cashBalance"));
-            try {
-                String newInput = sc.nextLine();
-                ammunition = Integer.parseInt(newInput);
-            } catch (Exception e) {
-                System.out.println("Please enter a number between 0 and " + supplyBalances.get("cashBalance"));
-            }
-
-            supplyBalances.put("ammunition", ammunition);
-        }
-
-        while (clothing < 0 || clothing > supplyBalances.get("cashBalance")) {
-            System.out.println("\nCURRENT CASH BALANCE: " + supplyBalances.get("cashBalance"));
-            System.out.println("HOW MUCH DO YOU WANT TO SPEND ON CLOTHING? ENTER BETWEEN 0-" +
-                    supplyBalances.get("cashBalance"));
-            try {
-                String newInput = sc.nextLine();
-                clothing = Integer.parseInt(newInput);
-            } catch (Exception e) {
-                System.out.println("Please enter a number between 0 and " + supplyBalances.get("cashBalance"));
-            }
-
-            supplyBalances.put("clothing", clothing);
-        }
-
-        while (misc < 0 || misc > supplyBalances.get("cashBalance")) {
-            System.out.println("\nCURRENT CASH BALANCE: " + supplyBalances.get("cashBalance"));
-            System.out.println("HOW MUCH DO YOU WANT TO SPEND ON MISCELLANEOUS SUPPLIES? ENTER BETWEEN 0-" +
-                    supplyBalances.get("cashBalance"));
-            try {
-                String newInput = sc.nextLine();
-                misc = Integer.parseInt(newInput);
-            } catch (Exception e) {
-                System.out.println("Please enter a number between 0 and " + supplyBalances.get("cashBalance"));
-            }
-
-            supplyBalances.put("misc", misc);
-        }
-
-        return supplyBalances;
     }
 
     // todo implement this
@@ -271,40 +168,40 @@ public class Main {
         return newSupplyBalances;
     }
 
-    public static void travel(Map<String, Integer> supplyBalances, Scanner sc, int marksmanship, int milesToGo) {
-        Map<String, Integer> newSupplyBalances = new HashMap<>(supplyBalances);
-        boolean illness = false;
-        boolean injury = false;
-        boolean blizzard = false;
-        boolean insufficientClothing = false;
-        int currentMilesToGo = 2040;
+    public static Traveler travel(Traveler traveler, Scanner sc) {
+        boolean hasVisitedFort = false;
+        boolean hasHunted = false;
         int choice = 0;
 
-        if (newSupplyBalances.get("food") < 13) {
-            System.out.println("\nYOU'D BETTER DO SOME HUNTING OR BUY FOOD AND SOON!!!!");
+        if (traveler.getSupplyBalances().get("food") < 13) {
+            System.out.println("You'd better do some hunting or buy food and soon!!!");
         }
 
-        System.out.println("DO YOU WANT TO (1) STOP AT THE NEXT FORT, (2) HUNT, OR (3) CONTINUE TRAVELING?");
+        System.out.println("\nDo you want to (1) stop at the next fort, (2) hunt, or (3) continue traveling?");
 
-        while (choice < 1 || choice > 3) {
-            try {
-                String input = sc.nextLine();
-                choice = Integer.parseInt(input);
-            } catch (Exception e) {
-                System.out.println("Please enter a number between 1 and 3");
+        while (choice != 3) {
+            while (choice < 1 || choice > 3) {
+                try {
+                    String input = sc.nextLine();
+                    choice = Integer.parseInt(input);
+                } catch (Exception e) {
+                    System.out.println("Please enter a number between 1 and 3");
+                }
+            }
+
+            switch (choice) {
+                case 1 -> {
+                    hasVisitedFort = true;
+                    traveler.buyFortSupplies();
+                }
+                case 2 -> {
+                    hasHunted = true;
+                    traveler.hunt();
+                }
             }
         }
 
-        switch (choice) {
-            case 1 -> newSupplyBalances = new HashMap<>(buyFortSupplies(newSupplyBalances, sc));
-            case 2 -> {
-                newSupplyBalances = new HashMap<>(hunt(newSupplyBalances, sc));
-                newSupplyBalances = new HashMap<>(eat(newSupplyBalances, sc));
-                milesToGo = dailyTravel(milesToGo, newSupplyBalances);
-
-            }
-        }
-
+        return traveler;
     }
 
 }
---
+++ src/main/java/org/example/Traveler.java
@@ -0,0 +1,201 @@
+package org.example;
+
+import java.util.Map;
+import java.util.Scanner;
+
+public class Traveler {
+    private int marksmanship;
+    private int milesTraveled = 0;
+    private Map<String, Integer> supplyBalances;
+    private Map<String, Boolean> booleanFlags;
+    private Scanner sc = new Scanner(System.in);
+
+    public Traveler() {
+        setSupplyBalances();
+        setBooleanFlags();
+    }
+
+    public int getMarksmanship() {
+        return marksmanship;
+    }
+
+    public int setMarksmanship() {
+        marksmanship = 0;
+
+        System.out.println("How good a shot are you with your rifle?");
+        System.out.println("(1) Ace marksman");
+        System.out.println("(2) Good shot");
+        System.out.println("(3) Fair to middlin'");
+        System.out.println("(4) Need more practice");
+        System.out.println("(5) Shaky knees");
+        System.out.println("Enter on of the above -- the better you claim you are, the faster you'll have to be " +
+                "with your gun to be successful.");
+
+        while (marksmanship < 1 || marksmanship > 5) {
+            try {
+                String newInput = sc.nextLine();
+                marksmanship = Integer.parseInt(newInput);
+                if (marksmanship < 1 || marksmanship > 5) throw new IllegalArgumentException();
+            } catch (Exception e) {
+                System.out.println("Please enter a number between 1 and 5...");
+            }
+        }
+
+        return marksmanship;
+    }
+
+    public int getMilesTraveled() {
+        return milesTraveled;
+    }
+
+    public void setMilesTraveled(int milesTraveled) {
+        this.milesTraveled = milesTraveled;
+    }
+
+    public Map<String, Integer> getSupplyBalances() {
+        return supplyBalances;
+    }
+
+    public void setSupplyBalances() {
+        this.supplyBalances.put("oxen", -1);
+        this.supplyBalances.put("food", -1);
+        this.supplyBalances.put("ammunition", -1);
+        this.supplyBalances.put("clothing", -1);
+        this.supplyBalances.put("misc", -1);
+    }
+
+    public Map<String, Boolean> getBooleanFlags() {
+        return booleanFlags;
+    }
+
+    public void setBooleanFlags() {
+        this.booleanFlags.put("insuffClothing", false);
+        this.booleanFlags.put("clearedSouthPass", false);
+        this.booleanFlags.put("clearedBlueMountains", false);
+        this.booleanFlags.put("hasInjury", false);
+        this.booleanFlags.put("hasIllness", false);
+        this.booleanFlags.put("inBlizzard", false);
+    }
+
+    public void buyInitialSupplies() {
+        int oxen = -1;
+        int food = -1;
+        int ammunition = -1;
+        int clothing = -1;
+        int misc = -1;
+
+        System.out.println("\nCurrent cash balance: " + supplyBalances.get("cashBalance"));
+        System.out.println("How much do you want to spend on food? Enter between 0-" +
+                supplyBalances.get("cashBalance"));
+
+        while (oxen < 200 || oxen > 300) {
+            System.out.println("\nCurrent cash balance: " + supplyBalances.get("cashBalance"));
+            System.out.println("\nHow much do you want to spend on your oxen team? Enter between 200-300.");
+
+            try {
+                String newInput = sc.nextLine();
+                oxen = Integer.parseInt(newInput);
+                if (oxen < 200 || oxen > 300) {
+                    throw new IllegalArgumentException("Please enter a number between 200 and 300...");
+                }
+                supplyBalances.put("oxen", oxen);
+                supplyBalances.put("cashBalance", supplyBalances.get("cashBalance") - oxen);
+            } catch (Exception e) {
+                System.out.println(e.getMessage());
+            }
+        }
+
+        while (food < 0 || food > supplyBalances.get("cashBalance")) {
+            System.out.println("\nCurrent cash balance: " + supplyBalances.get("cashBalance"));
+            System.out.println("\nHow much do you want to spend on your oxen team? Enter between 0- " +
+                    supplyBalances.get("cashBalance"));
+
+            try {
+                String newInput = sc.nextLine();
+                food = Integer.parseInt(newInput);
+                if (food < 0 || food > supplyBalances.get("cashBalance")) {
+                    throw new IllegalArgumentException("Please enter a number between 0 and " +
+                            supplyBalances.get("cashBalance"));
+                }
+                supplyBalances.put("food", food);
+                supplyBalances.put("cashBalance", supplyBalances.get("cashBalance") - food);
+            } catch (Exception e) {
+                System.out.println(e.getMessage());
+            }
+
+            supplyBalances.put("food", food);
+        }
+
+        while (ammunition < 0 || ammunition > supplyBalances.get("cashBalance")) {
+            System.out.println("\nCurrent cash balance: " + supplyBalances.get("cashBalance"));
+            System.out.println("How much do you want to spend on ammunition? Enter between 0-" +
+                    supplyBalances.get("cashBalance") + ". Remember, each dollar spent buys 50 bullets.");
+
+            try {
+                String newInput = sc.nextLine();
+                ammunition = Integer.parseInt(newInput);
+                if (ammunition < 0 || ammunition > supplyBalances.get("cashBalance")) {
+                    throw new IllegalArgumentException("Please enter a number between 0 and " +
+                            supplyBalances.get("cashBalance"));
+                }
+                supplyBalances.put("ammunition", ammunition);
+                supplyBalances.put("cashBalance", supplyBalances.get("cashBalance") - ammunition);
+            } catch (Exception e) {
+                System.out.println(e.getMessage());
+            }
+
+            supplyBalances.put("ammunition", ammunition * 50);
+        }
+
+        while (clothing < 0 || clothing > supplyBalances.get("cashBalance")) {
+            System.out.println("\nCurrent cash balance: " + supplyBalances.get("cashBalance"));
+            System.out.println("How much do you want to spend on clothing? Enter between 0-" +
+                    supplyBalances.get("cashBalance"));
+
+            try {
+                String newInput = sc.nextLine();
+                clothing = Integer.parseInt(newInput);
+                if (clothing < 0 || clothing > supplyBalances.get("cashBalance")) {
+                    throw new IllegalArgumentException("Please enter a number between 0 and " +
+                            supplyBalances.get("cashBalance"));
+                }
+                supplyBalances.put("clothing", clothing);
+                supplyBalances.put("cashBalance", supplyBalances.get("cashBalance") - clothing);
+            } catch (Exception e) {
+                System.out.println(e.getMessage());
+            }
+
+            supplyBalances.put("clothing", clothing);
+        }
+
+        while (misc < 0 || misc > supplyBalances.get("cashBalance")) {
+            System.out.println("\nCurrent cash balance: " + supplyBalances.get("cashBalance"));
+            System.out.println("How much do you want to spend on miscellaneous supplies? Enter between 0-" +
+                    supplyBalances.get("cashBalance"));
+
+            try {
+                String newInput = sc.nextLine();
+                misc = Integer.parseInt(newInput);
+                if (misc < 0 || misc > supplyBalances.get("cashBalance")) {
+                    throw new IllegalArgumentException("Please enter a number between 0 and " +
+                            supplyBalances.get("cashBalance"));
+                }
+                supplyBalances.put("misc", misc);
+                supplyBalances.put("cashBalance", supplyBalances.get("cashBalance") - misc);
+            } catch (Exception e) {
+                System.out.println(e.getMessage());
+            }
+
+            supplyBalances.put("misc", misc);
+        }
+    }
+
+    public void buyFortSupplies() {
+
+    }
+
+    public void hunt() {
+
+    }
+
+}
